c2ae7b9579a125a3989124f63343c1a4f2d3c40f
Lucas Yang <lucas@fionas-macbook-pro.local>
2017-05-25 11:01:28 -0400

    Initial Commit

---
+++ FirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApplication
+//
+//  Created by Lucas Yang on 2017-05-25.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FirstApplication
+//
+//  Created by Lucas Yang on 2017-05-25.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
